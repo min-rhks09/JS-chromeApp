1ab8e69ebbf23222f0a868f435276bfec3e7b2d1
min-rhks09 <110644082+min-rhks09@users.noreply.github.com>
2022-12-22 22:17:53 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const calculator = {
+    add: function(a, b){
+        console.log(a + b);
+    },
+    minus: function(a, b){
+        console.log(a - b);
+    },
+    divide: function(a, b){
+        console.log(a / b);
+    },
+    power: function(a, b){
+        console.log(a ** b);
+    },
+};
+
+calculator.add(1, 2);
+
+calculator.minus(3, 2);
+
+calculator.divide(2, 2);
+
+calculator.power(2, 2);
