9f0898e407173765b26bd62794141cf3d3640299
min-rhks09 <110644082+min-rhks09@users.noreply.github.com>
2023-01-29 23:41:43 +0900

    0129

--- app.js
+++ app.js
@@ -5,5 +5,4 @@ const h1 = document.querySelector("div.hello:first-child h1");
 function handleTitleClick(){
     h1.classList.toggle("change");
 };
-
 h1.addEventListener("click", handleTitleClick);
