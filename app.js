300183080b2fa7156a6ae747f75c06e765dcb04a
min_rhks09 <220805min@gmail.com>
2023-02-01 15:32:20 +0900

    0201

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-h1.addEventListener("click", handleTitleClick);
+// const loginForm = document.getElementById("login_form");
+// const loginInput = loginForm.querySelector("input");
+// const loginButton = loginForm.querySelector("button");
 
-//toggle을 이용하여 add, remove 대체하기
-const h1 = document.querySelector("div.hello:first-child h1");
-function handleTitleClick(){
-    h1.classList.toggle("change");
+//위의 코드를 더욱 간단히
+const loginInput = document.querySelector("#login_form input");
+const loginButton = document.querySelector("#login_form button");
+
+function handleLoginButtonClick(){
+    console.log("hello", loginInput.value)
+    
+    //console.log(loginInput.value) //input창에 입력내용 콘솔에 출력
+    //console.log("btnclick"); //버튼 클릭시 콘솔에 출력
 };
-h1.addEventListener("click", handleTitleClick);
+
+loginButton.addEventListener("click", handleLoginButtonClick);
+
--- pr1.js
+++ pr1.js
@@ -213,3 +213,11 @@ else if(age > 50){
 //     };
 // };
 
+// h1.addEventListener("click", handleTitleClick);
+
+// //toggle을 이용하여 add, remove 대체하기
+// const h1 = document.querySelector("div.hello:first-child h1");
+// function handleTitleClick(){
+//     h1.classList.toggle("change");
+// };
+// h1.addEventListener("click", handleTitleClick);
