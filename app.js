1fb605eabf52adf088f4c892e96ba38a1017281e
min_rhks09 <220805min@gmail.com>
2023-02-06 22:51:07 +0900

    0206

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
 const loginForm = document.querySelector("#login_form");
 const loginInput = document.querySelector("#login_form input");
 const greeting = document.querySelector("#greeting");
+
 const HIDDEN_CLASSNAME = "hidden"; //string만 포함된 변수는 대문자로 표기하고 string을 저장하는 역할을 수행
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){
     event.preventDefault();
     //preventDefault를 이용하여 브라우저가 기본동작을 실행하지 못하도록 막을 수 있음
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, username); //localstorage의 DB에 username을 저장해주는 기능
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    console.log(username);
+    localStorage.setItem(USERNAME_KEY, username); //localstorage의 DB에 username을 저장해주는 기능
     //greeting.innerText = "Hi, " + username;
-    paintGreetings(usernameData);
+    paintGreetings();
 };
 
-function paintGreetings(username){
+function paintGreetings(){
     greeting.innerText = `Hi, ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 };
@@ -28,6 +27,6 @@ if(usernameData === null){
     loginForm.addEventListener("submit", onLoginSubmit);
 }else{
     //greeting 보여주기
-    paintGreetings(usernameData);   
+    paintGreetings();   
 };
 
